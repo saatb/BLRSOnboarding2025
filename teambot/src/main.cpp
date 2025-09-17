ba4c8f3212eb5dbc2db990b097c3f318870c98fc
itsdragonic <fzawislak25@lrchs.org>
2025-09-17 14:34:36 -0400

    added basic line sensor logic

--- teambot/src/main.cpp
+++ teambot/src/main.cpp
@@ -1,5 +1,11 @@
 #include "main.h"
 
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup left_mg({-1});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+pros::MotorGroup right_mg({6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+pros::Motor intake(16);
+pros::adi::AnalogIn line_tracker('A');
+
 /**
  * A callback function for LLEMU's center button.
  *
@@ -58,7 +64,18 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	while (true) {
+		master.print(0,0, "Light: %.2d", line_tracker.get_value());
+		if (line_tracker.get_value() < 2800) {
+			left_mg.move(80);
+			right_mg.move(80);
+		} else {
+			left_mg.move(0);
+			right_mg.move(0);
+		}
+	}
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -74,12 +91,9 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
-
 	while (true) {
+		master.print(0,0, "Light: %.2d", line_tracker.get_value());
+
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
@@ -90,5 +104,13 @@ void opcontrol() {
 		left_mg.move(dir - turn);                      // Sets left motor voltage
 		right_mg.move(dir + turn);                     // Sets right motor voltage
 		pros::delay(20);                               // Run for 20 ms then update
+
+		if (master.get_digital(DIGITAL_A)) {
+			intake.move(-100);
+		} else if (master.get_digital(DIGITAL_B)) {
+			intake.move(100);
+		} else {
+			intake.move(0);
+		}
 	}
 }
